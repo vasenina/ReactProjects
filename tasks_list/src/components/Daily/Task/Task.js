47eef375693ef371ab88f1a4c4acc6dc9513f24f
Julia Vasenina <mamicheva@gmail.com>
2020-10-26 23:00:39 +0100

    gradient colors

--- tasks_list/src/components/Daily/Task/Task.js
+++ tasks_list/src/components/Daily/Task/Task.js
@@ -7,20 +7,16 @@ const task = (props)=>{
    
     let classNames = classes.Task ;
     let style = {};
-    //let border =props.color;
     let back =  props.color;
     (props.done) ? 
       (classNames += ' ' + classes.Task_done ):
       (style = {background: back});
-      //(style = {'border-color': border});
-      //border-color:
-      //(style = {background: props.color});
-    
     
         return(
                
             <div  className ={classNames} style = {style} onClick = {props.editing}>
-                    <input type = "checkbox" checked = {props.done} onClick = {props.check}/>
+                    <input type = "checkbox" className = {classes.check} checked = {props.done} onClick = {props.check}/>
+        
                     <label > {props.todo} </label>
             </div>
      
